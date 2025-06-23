e325b6949911fc0d1dca3684e15f6bcb2017fe66
TheFloatBoat <139195356+TheFloatBoat@users.noreply.github.com>
2025-06-23 20:27:58 +0800

    Update CSV format changer.py to use my automated location CSV maker

--- CSV format changer.py
+++ CSV format changer.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+'''NOTE: THIS MATRIX APPEARS TO BE BUGGY'''
+#matrix
 key = pd.read_csv("random_25_locations.csv")["name"].to_list()
 distanceMatrix = pd.read_csv("distance_matrix_with_coordinates.csv")[["Origin Postal Code","Destination Postal Code","Distance (meters)","Duration (seconds)"]]
 newDistanceMatrix = np.zeros((len(key),len(key)))
@@ -7,11 +9,17 @@ for _, row in distanceMatrix.iterrows():
     newDistanceMatrix[key.index(row["Origin Postal Code"])][key.index(row["Destination Postal Code"])] = row["Distance (meters)"]
 pd.DataFrame(newDistanceMatrix).to_csv("newDistanceMatrix.csv")
 
-
-#todo:automate this
+#location csv
 googleCSV = pd.read_csv("random_25_locations.csv")
-X=googleCSV["lat"].values *110.574
-Y = googleCSV["lng"].values *111.320*np.cos(np.radians(googleCSV["lat"]))
+X=((googleCSV["lat"].values-1.1) *110.574*1000).astype(int)
+Y = ((googleCSV["lng"].values-103.6) *111.320*np.cos(np.radians(googleCSV["lat"]))*1000).astype(int)
 
-print(X)
-print(Y)
+formatted=np.ones((len(X),5)).astype(int)
+formatted[:,0] = np.arange(1,len(X)+1)
+formatted[:,2] = X
+formatted[:,3] = Y
+formatted[:,4] = np.random.rand(1,len(X))*10
+
+formatted = pd.DataFrame(formatted)
+formatted.columns = ["NodeNumber","NodeType","X","Y","Demand"]
+formatted.to_csv("random_25_locations_FORMATTED.csv")
